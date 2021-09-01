3cbb3cf9d2f359be6c6a9c653f9d0ec0c01929f0
Aditya Pathak <mynewchilly@gmail.com>
2021-09-01 16:29:16 +0530

    Re

--- block-BRaaaa/code/script.js
+++ block-BRaaaa/code/script.js
@@ -1,8 +1,27 @@
 const input = document.querySelector("input");
 const root = document.querySelector(".movie-list");
-let allMovies = [];
+const allMovies = [];
+const elm = (type, attr = {}, ...children) => {
+  const element = document.createElement(type);
+  const keyArr = Object.keys(attr);
+  keyArr.forEach((key) => {
+    if (key.startsWith("data-")) {
+      element.setAttribute(key, attr[key]);
+    } else {
+      element[key] = attr[key];
+    }
+  });
+  children.forEach((child) => {
+    if (typeof child === "object") {
+      element.append(child);
+    } else if (typeof child === "string") {
+      const node = document.createTextNode(child);
+      element.append(node);
+    }
+  });
+  return element;
+};
 input.addEventListener("keyup", (e) => {
-  //adding a movie
   if (e.keyCode === 13) {
     allMovies.push({
       name: e.target.value,
@@ -12,6 +31,37 @@ input.addEventListener("keyup", (e) => {
     e.target.value = "";
   }
 });
+const createUI = (allMovies) => {
+  root.innerHTML = "";
+  allMovies.forEach((movie, i) => {
+    let li = elm(
+      "li",
+      {},
+      elm("cite", {}, movie.name),
+      elm(
+        "button",
+        {
+          "data-id": i,
+        },
+        movie.watched ? "Watched" : "Yet to Watch"
+      )
+    );
+    root.append(li);
+  });
+  const buttons = document.querySelectorAll("button");
+  buttons.forEach((button) => {
+    button.addEventListener("click", handleBtn);
+  });
+  const cites = document.querySelectorAll("cite");
+  cites.forEach((cite) => {
+    if (!movie.watched) {
+      cite.style.textDecoration = "none";
+    } else {
+      cite.style.textDecoration = "line-through";
+    }
+  });
+};
+
 var handleBtn = (event) => {
   let id = event.target.dataset.id;
   if (!allMovies[id].watched) {
@@ -20,24 +70,4 @@ var handleBtn = (event) => {
     allMovies[id].watched = false;
   }
   createUI(allMovies);
-};
-const createUI = (allMovies) => {
-  root.innerHTML = "";
-  allMovies.forEach((movie, i) => {
-    let li = document.createElement("li");
-    let cite = document.createElement("cite");
-    cite.innerText = movie.name;
-    let button = document.createElement("button");
-    if (!movie.watched) {
-      button.innerText = "To Watch";
-      cite.style.textDecoration = "none";
-    } else {
-      button.innerText = "Watched";
-      cite.style.textDecoration = "line-through";
-    }
-    button.setAttribute("data-id", i);
-    li.append(cite, button);
-    root.append(li);
-    button.addEventListener("click", handleBtn);
-  });
 };
